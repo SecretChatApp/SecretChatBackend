f7102bfd03f81ddaf6bc6baf380aa51b1306c08e
DiazNugraha <diaznugraha00@gmail.com>
2023-09-03 21:38:09 +0700

    update: change access token check

--- app/middlewares/jwt.go
+++ app/middlewares/jwt.go
@@ -4,6 +4,7 @@ import (
 	"backend/app/config"
 	"backend/app/helpers"
 	"context"
+	"strings"
 
 	// "context"
 
@@ -14,19 +15,21 @@ import (
 
 func JWTMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		c, err := r.Cookie("token")
-		if err != nil {
-			if err == http.ErrNoCookie {
-				response := map[string]string{
-					"message": "User unauthorized",
-				}
 
-				helpers.ResponseJSON(w, http.StatusUnauthorized, response)
-				return
+		c := r.Header.Get("Authorization")
+		splitToken := strings.Split(c, " ")
+		accessToken := splitToken[1]
+
+		if accessToken == "" {
+			response := map[string]string{
+				"message": "User unauthorized",
 			}
+
+			helpers.ResponseJSON(w, http.StatusUnauthorized, response)
+			return
 		}
 
-		tokenString := c.Value
+		tokenString := accessToken
 		claims := &config.JWTClaim{}
 
 		token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
